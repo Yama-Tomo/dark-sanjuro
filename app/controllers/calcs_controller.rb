d89864e21917319fdb58454e35a0a8542aee61d2
Yama-Tomo <mazda13bturbo@gmail.com>
2014-07-29 11:06:30 +0900

    減衰率を任意の値で計算できるように修正

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,13 +1,24 @@
 class CalcsController < ApplicationController
   def calc
     @calc = Calc.new 
+
+    # フォームデフォルト値を設定
+    @calc.relay_distance_attenuation          = 0.5
+    @calc.connector_num_per_builing           = 2
+    @calc.bilding_num_attenuation             = 0.5
+    @calc.admission_distance_attenuation      = 0.5
+    @calc.start_and_end_point_num_attenuation = 0.5
+    @calc.start_and_end_point_num             = 2
   end
  
   def exec_calc 
     @calc = Calc.new params[:calc]
 
     # 減衰値を計算
-    @attenuation = ((@calc.relay_distance.to_f * 0.5) + (@calc.building_num.to_f) + (@calc.admission_distance.to_f * 0.5) + (@calc.connector_num.to_f))
+    @attenuation = ((@calc.relay_distance.to_f * @calc.relay_distance_attenuation.to_f) \
+                 + (@calc.building_num.to_f * @calc.connector_num_per_builing.to_f * @calc.bilding_num_attenuation.to_f) \
+                 + (@calc.admission_distance.to_f * @calc.admission_distance_attenuation.to_f) \
+                 + (@calc.start_and_end_point_num_attenuation.to_f * @calc.connector_num.to_f * @calc.start_and_end_point_num.to_f))
 
     @media_converters = MediaConverter.where(["bandwidth = ? AND trans_lost_from <= ? AND trans_lost_to >= ?", @calc.bandwidth, @attenuation, @attenuation])
                                       .order("bandwidth DESC, manufacturer ASC, name ASC")
--- app/models/calc.rb
+++ app/models/calc.rb
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 class Calc < NonActiveRecord::Base
-  attr_accessor :relay_distance, :building_num, :admission_distance, :connector_num, :bandwidth
+  attr_accessor :relay_distance, :building_num, :admission_distance, :connector_num, :bandwidth,
+                :relay_distance_attenuation, :bilding_num_attenuation, :connector_num_per_builing, :admission_distance_attenuation,
+                :start_and_end_point_num, :start_and_end_point_num_attenuation
 end
 
